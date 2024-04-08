7f7d4572790af96f8d570c9a1c9aaabc7d2890bd
KimEunHye <gpqjr00@naver.com>
2024-04-09 00:28:17 +0900

    Feat: 웹 폰트 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Gaegu } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Gaegu({ subsets: ["latin"], weight:['400'] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
