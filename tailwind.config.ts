3af9e4dd85cce56fefd3f57d6bccf57bad7edaf8
KimEunHye <gpqjr00@naver.com>
2024-04-09 00:28:28 +0900

    Feat: 필요한 css 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,34 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontSize: {
+        rootSize: '64px',
+      },
+      width: {
+        buttonWidth: '190px',
+      },
+      height: {
+        buttonHeight: '40px',
+      },
+      backgroundColor: {
+        lightBrown: '#EDD2B4',
+        brown: '#D0A97E',
+      },
+      borderRadius: {
+        radiusTenPx: '10px'
+      }
     },
   },
   plugins: [],
