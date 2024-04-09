007de59412396b54ed46b62c8e03988b0f7b5c55
KimEunHye <gpqjr00@naver.com>
2024-04-10 01:56:17 +0900

    Feat: Link 컴포넌트 추가 및 Button 컴포넌트 적용

--- src/app/(beforeLogin)/page.tsx
+++ src/app/(beforeLogin)/page.tsx
@@ -1,18 +1,20 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 import MonyangIcon from '/public/monyang-i.png';
+import Button from './_component/Button';
 
 const BeforeLoginPage = () => {
   return (
-    <div className='flex justify-center flex-col items-center h-screen'>
+    <div className="flex justify-center flex-col items-center h-screen">
       <Image src={MonyangIcon} alt="모냥이 아이콘" width={150} height={150} />
-      <div className='text-rootSize'>모냥이</div>
-      <div className='my-2'>
-        <button className='w-buttonWidth h-buttonHeight bg-lightBrown rounded-radiusTenPx text-2xl'>로그인</button>
-      </div>
-      <div className='my-2'>
-        <button className='w-buttonWidth h-buttonHeight bg-lightBrown rounded-radiusTenPx text-2xl'>회원가입</button>
-      </div>
+      <div className="text-rootSize">모냥이</div>
+      <Link href="/signin" className="my-2">
+        <Button text="로그인" />
+      </Link>
+      <Link href="/signup" className="my-2">
+        <Button text="회원가입" />
+      </Link>
     </div>
   );
 };
