9560de8f01e5174a14bd0b08eeebd3cbdcb49375
KimEunHye <gpqjr00@naver.com>
2024-04-12 00:25:29 +0900

    Design: FormBackground flex-column 적용

--- src/app/(beforeLogin)/_component/FormBackground.tsx
+++ src/app/(beforeLogin)/_component/FormBackground.tsx
@@ -3,6 +3,11 @@ import { ReactNode } from 'react';
 type Children = {
   children: ReactNode;
 };
+
 export default function FormBackground({ children }: Children) {
-  return <div className='w-formWidth h-formHeight bg-lightBrown flex justify-center items-center rounded-radiusTenPx my-form'>{children}</div>;
+  return (
+    <div className="w-formWidth h-formHeight bg-lightBrown flex flex-col justify-center rounded-radiusTenPx my-form p-input rounded-radius10">
+      {children}
+    </div>
+  );
 }
