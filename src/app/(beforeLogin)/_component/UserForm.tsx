7916d13783c47bf42651d5bf3075340b45a8a26c
KimEunHye <gpqjr00@naver.com>
2024-04-12 00:26:56 +0900

    Feat: 사용자 정보 입력 컴포넌트 생성

---
+++ src/app/(beforeLogin)/_component/UserForm.tsx
@@ -0,0 +1,48 @@
+'use client';
+import { useRef } from 'react';
+
+import Button from './Button';
+import FormTitle from './FormTitle';
+import InputBox from './InputBox';
+import FormBackground from './FormBackground';
+
+export default function UserForm() {
+  const idRef = useRef<HTMLInputElement>(null);
+  const nameRef = useRef<HTMLInputElement>(null);
+  const pwRef = useRef<HTMLInputElement>(null);
+  const profileRef = useRef<HTMLInputElement>(null);
+
+  const onSubmit = (e: React.FormEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    console.log(idRef.current?.value);
+    console.log(nameRef.current?.value);
+    console.log(pwRef.current?.value);
+    console.log(profileRef.current?.value);
+  };
+
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <FormBackground>
+          <div className="m-formTitle">
+            <FormTitle text="아이디" forName="id" />
+            <InputBox type="text" id="id" ref={idRef} />
+          </div>
+          <div className="m-formTitle">
+            <FormTitle text="닉네임" forName="name" />
+            <InputBox type="text" id="name" ref={nameRef} />
+          </div>
+          <div className="m-formTitle">
+            <FormTitle text="비밀번호" forName="password" />
+            <InputBox type="password" id="password" ref={pwRef} />
+          </div>
+          <div className="m-formTitle">
+            <FormTitle text="프로필 사진" forName="profile_image" />
+            <InputBox type="file" id="profile_image" ref={profileRef} />
+          </div>
+        </FormBackground>
+        <Button type="submit" text="로그인하기" />
+      </form>
+    </>
+  );
+}
