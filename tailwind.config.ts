fbc074570f45c043ea3b8ae81e0af7775fbb11e8
KimEunHye <gpqjr00@naver.com>
2024-04-17 23:55:06 +0900

    Feat: css 속성 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,20 +17,21 @@ const config: Config = {
         rootSize: '64px',
       },
       width: {
-        buttonWidth: '285px',
-        formWidth: '335px',
-        inputWidth: '175px',
-        formTitleWidth: '85px',
+        buttonWidth: '400px',
+        formWidth: '400px',
+        inputWidth: '220px',
+        formTitleWidth: '110px',
       },
       height: {
         buttonHeight: '60px',
-        formHeight: '295px',
+        formHeight: '350px',
         inputHeight: '30px',
         headerHeight: '55px',
       },
       backgroundColor: {
         lightBrown: '#EDD2B4',
         brown: '#D0A97E',
+        modal: 'rgba(24, 24, 27,0.5)',
       },
       borderRadius: {
         radius10: '10px',
@@ -39,7 +40,7 @@ const config: Config = {
       margin: {
         form: '50px',
         input: '15px',
-        formTitle: '10px 5px',
+        formTitle: '25px 5px',
       },
       padding: {
         input: '30px',
