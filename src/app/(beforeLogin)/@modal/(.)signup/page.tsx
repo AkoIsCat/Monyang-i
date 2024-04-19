fe8eea3bfce9d0e17a3840d820a1d873767e32c2
KimEunHye <gpqjr00@naver.com>
2024-04-19 23:36:00 +0900

    Feat: 모달창에는 뒤로가기를 X로 표시하도록 수정

--- src/app/(beforeLogin)/@modal/(.)signup/page.tsx
+++ src/app/(beforeLogin)/@modal/(.)signup/page.tsx
@@ -1,11 +1,11 @@
-import BackPageButton from '../../_component/BackPageButton';
+import BackPageButtonX from '../../_component/BackPageButtonX';
 import SignUpUserForm from '../../_component/SignUpUserForm';
 
 export default function SignUpModal() {
   return (
     <div className="w-screen h-screen flex flex-col justify-between absolute inset-0 bg-modal">
       <header className="h-headerHeight px-back">
-        <BackPageButton />
+        <BackPageButtonX />
       </header>
       <div className="flex justify-center flex-col items-center grow">
         <SignUpUserForm />
