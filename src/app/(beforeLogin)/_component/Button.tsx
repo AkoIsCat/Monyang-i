38b45a0be32732b34fc7b48334bde7392b75d240
KimEunHye <gpqjr00@naver.com>
2024-04-23 00:35:26 +0900

    Design: hover 시 그림자 효과 추가

--- src/app/(beforeLogin)/_component/Button.tsx
+++ src/app/(beforeLogin)/_component/Button.tsx
@@ -5,7 +5,10 @@ type Type = {
 
 export default function Button({ text, type }: Type) {
   return (
-    <button type={type} className="w-buttonWidth h-buttonHeight bg-lightBrown rounded-radius20 text-2xl">
+    <button
+      type={type}
+      className="w-buttonWidth h-buttonHeight bg-lightBrown rounded-radius20 text-2xl hover:shadow-buttonShadow"
+    >
       {text}
     </button>
   );
