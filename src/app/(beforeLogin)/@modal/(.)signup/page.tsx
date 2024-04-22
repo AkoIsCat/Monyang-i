2d11d8f84ca765fd7dc470924e62b0cb029afa29
KimEunHye <gpqjr00@naver.com>
2024-04-23 00:34:58 +0900

    Feat: form에 뒤로가기 버튼을 추가했으므로 page 내의 뒤로가기 버튼은 제거

--- src/app/(beforeLogin)/@modal/(.)signup/page.tsx
+++ src/app/(beforeLogin)/@modal/(.)signup/page.tsx
@@ -4,9 +4,7 @@ import SignUpUserForm from '../../_component/SignUpUserForm';
 export default function SignUpModal() {
   return (
     <div className="w-screen h-screen flex flex-col justify-between absolute inset-0 bg-modal">
-      <header className="h-headerHeight px-back">
-        <BackPageButtonX />
-      </header>
+      <header className="h-headerHeight px-back"></header>
       <div className="flex justify-center flex-col items-center grow">
         <SignUpUserForm />
       </div>
