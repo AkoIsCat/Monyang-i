44e4f81d95b6745ff6c6650c66afedefeb869784
KimEunHye <gpqjr00@naver.com>
2024-04-24 01:03:56 +0900

    Refactor: 중복된 태그 밖으로 꺼냄

--- src/app/(afterLogin)/_component/NavMenu.tsx
+++ src/app/(afterLogin)/_component/NavMenu.tsx
@@ -21,89 +21,97 @@ export default function NavMenu() {
   return (
     <ul className="text-navSize">
       <li className="my-nav">
-        <Link href="/home">
-          {segment === 'home' ? (
-            <div className="flex">
-              <div>
-                <HomeColorIcon />
-              </div>
-              <div className="font-bold mx-li">홈</div>
-            </div>
-          ) : (
-            <div className="flex">
-              <div>
-                <HomeIcon />
-              </div>
-              <div className="mx-li">홈</div>
-            </div>
-          )}
+        <Link href="/home" className="inline-block">
+          <div className="flex">
+            {segment === 'home' ? (
+              <>
+                <div>
+                  <HomeColorIcon />
+                </div>
+                <div className="font-bold mx-li">홈</div>
+              </>
+            ) : (
+              <>
+                <div>
+                  <HomeIcon />
+                </div>
+                <div className="mx-li">홈</div>
+              </>
+            )}
+          </div>
         </Link>
       </li>
       <li className="my-nav">
-        <Link href="/search">
-          {segment === 'search' ? (
-            <div className="flex">
-              <div>
-                <CompassColorIcon />
-              </div>
-              <div className="font-bold mx-li">탐색</div>
-            </div>
-          ) : (
-            <div className="flex">
-              <div>
-                <CompassIcon />
-              </div>
-              <div className="mx-li">탐색</div>
-            </div>
-          )}
+        <Link href="/search" className="inline-block">
+          <div className="flex">
+            {segment === 'search' ? (
+              <>
+                <div>
+                  <CompassColorIcon />
+                </div>
+                <div className="font-bold mx-li">탐색</div>
+              </>
+            ) : (
+              <>
+                <div>
+                  <CompassIcon />
+                </div>
+                <div className="mx-li">탐색</div>
+              </>
+            )}
+          </div>
         </Link>
       </li>
       <li className="my-nav">
-        <Link href="/messages">
-          {segment === 'messages' ? (
-            <div className="flex">
-              <div>
-                <MessageColorIcon />
-              </div>
-              <div className="font-bold mx-li">쪽지</div>
-            </div>
-          ) : (
-            <div className="flex">
-              <div>
-                <MessageIcon />
-              </div>
-              <div className="mx-li">쪽지</div>
-            </div>
-          )}
+        <Link href="/messages" className="inline-block">
+          <div className="flex">
+            {segment === 'messages' ? (
+              <>
+                <div>
+                  <MessageColorIcon />
+                </div>
+                <div className="font-bold mx-li">쪽지</div>
+              </>
+            ) : (
+              <>
+                <div>
+                  <MessageIcon />
+                </div>
+                <div className="mx-li">쪽지</div>
+              </>
+            )}
+          </div>
         </Link>
       </li>
       <li className="my-nav">
-        <Link href={`/${user.id}`}>
-          {segment === user.id ? (
-            <div className="flex">
-              <Image
-                src={user.image}
-                alt="user_image"
-                width={45}
-                height={45}
-                className="rounded-radiusCircle"
-                priority
-              />
-              <div className="font-bold mx-li">프로필</div>
-            </div>
-          ) : (
-            <div className="flex">
-              <Image
-                src={user.image}
-                alt="user_image"
-                width={45}
-                height={45}
-                className="rounded-radiusCircle"
-                priority
-              />
-              <div className="mx-li">프로필</div>
-            </div>
-          )}
+        <Link href={`/${user.id}`} className="inline-block">
+          <div className="flex">
+            {segment === user.id ? (
+              <>
+                <Image
+                  src={user.image}
+                  alt="user_image"
+                  width={45}
+                  height={45}
+                  className="rounded-radiusCircle"
+                  priority
+                />
+                <div className="font-bold mx-li">프로필</div>
+              </>
+            ) : (
+              <>
+                <Image
+                  src={user.image}
+                  alt="user_image"
+                  width={45}
+                  height={45}
+                  className="rounded-radiusCircle"
+                  priority
+                />
+                <div className="mx-li">프로필</div>
+              </>
+            )}
+          </div>
         </Link>
       </li>
     </ul>
