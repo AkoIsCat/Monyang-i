f9bcb14126bdebc2f82e3acf7ad64d4d3abd740f
KimEunHye <gpqjr00@naver.com>
2024-04-25 02:12:48 +0900

    Feat: SearchIcon svg 컴포넌트 생성

---
+++ src/app/(afterLogin)/_component/SearchIcon.tsx
@@ -0,0 +1,19 @@
+export default function SearchIcon() {
+  return (
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M14.3264 14.2974L18.3334 18.3333M16.4815 9.07403C16.4815 13.1651 13.1651 16.4814 9.07409 16.4814C4.98309 16.4814 1.66669 13.1651 1.66669 9.07403C1.66669 4.98303 4.98309 1.66663 9.07409 1.66663C13.1651 1.66663 16.4815 4.98303 16.4815 9.07403Z"
+        stroke="#A3A3A3"
+        strokeWidth="2.6936"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
