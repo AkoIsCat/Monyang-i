1ddd7348bdc4eccb9cf6648c18340a6b9ab518d3
KimEunHye <gpqjr00@naver.com>
2024-05-02 20:55:54 +0900

    Feat: section 태그 추가 및 여백 조정

--- src/app/(afterLogin)/layout.tsx
+++ src/app/(afterLogin)/layout.tsx
@@ -15,37 +15,43 @@ type Props = {
 export default function AfterLoginLayout({ children }: Props) {
   return (
     <div className="flex">
-      <div className="grow flex justify-center">
-        <div>
-          <div className="my-input">
-            <Link href="/home" className="inline-block px-right">
-              <Image
-                src={MonyangIcon}
-                alt="모냥이 아이콘"
-                width={58}
-                priority
-              />
-            </Link>
+      <section className="grow">
+        <div className="flex justify-end mr-[100px]">
+          <div>
+            <div className="my-input">
+              <Link href="/home" className="inline-block px-right">
+                <Image
+                  src={MonyangIcon}
+                  alt="모냥이 아이콘"
+                  width={58}
+                  priority
+                />
+              </Link>
+            </div>
+            <nav>
+              <NavMenu />
+            </nav>
           </div>
-          <nav>
-            <NavMenu />
-          </nav>
         </div>
-      </div>
-      <div className="w-contentsWidth grow-0">{children}</div>
-      <div className="grow px-padding15">
-        <div className="my-input">
-          <SearchForm />
+      </section>
+      <section>
+        <div className="w-contentsWidth grow-0">{children}</div>
+      </section>
+      <section className="grow">
+        <div className="px-padding15">
+          <div className="my-input">
+            <SearchForm />
+          </div>
+          <div className="w-searchWidth py-back">
+            <TitleBox>실시간 트렌드</TitleBox>
+            <TrendBox trend="고양이" />
+            <TrendBox trend="고양이" />
+            <TrendBox trend="고양이" />
+            <TrendBox trend="고양이" />
+            <TrendBox trend="고양이" />
+          </div>
         </div>
-        <div className="w-searchWidth py-back">
-          <TitleBox>실시간 트렌드</TitleBox>
-          <TrendBox trend="고양이" />
-          <TrendBox trend="고양이" />
-          <TrendBox trend="고양이" />
-          <TrendBox trend="고양이" />
-          <TrendBox trend="고양이" />
-        </div>
-      </div>
+      </section>
     </div>
   );
 }
