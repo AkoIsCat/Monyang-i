86beae595d707152261bc01f6874c0333c67534f
KimEunHye <gpqjr00@naver.com>
2024-05-02 22:47:55 +0900

    Feat: 팔로우 추천 컴포넌트 작성

--- src/app/(afterLogin)/_component/FollowBox.tsx
+++ src/app/(afterLogin)/_component/FollowBox.tsx
@@ -10,15 +10,24 @@ type Props = {
 
 export default function FollowBox({ image, userName, userId }: Props) {
   return (
-    <div>
-      <div>
-        <Image src={image} alt={`${userName} 프로필 이미지`} />
+    <div className="flex justify-between w-searchWidth h-followBoxHeight items-center border-b-1 border-lightBrownTP hover:bg-lightBrownTP cursor-pointer">
+      <div className="flex px-right">
+        <div>
+          <Image
+            src={image}
+            alt={`${userName} 프로필 이미지`}
+            width={50}
+            height={50}
+            priority
+            className="rounded-radiusCircle"
+          />
+        </div>
+        <div className="mx-[10px]">
+          <div className="text-trendSize">{userName}</div>
+          <div className="text-followIdSize text-id">@{userId}</div>
+        </div>
       </div>
-      <div>
-        <div>{userName}</div>
-        <div>{userId}</div>
-      </div>
-      <div>
+      <div className="px-right">
         <FollowButton />
       </div>
     </div>
