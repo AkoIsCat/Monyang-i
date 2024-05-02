281c55c75c68c31c0b39075733e434c45a2a958a
KimEunHye <gpqjr00@naver.com>
2024-05-02 22:48:03 +0900

    Feat: 팔로우 버튼 컴포넌트 작성

--- src/app/(afterLogin)/_component/FollowButton.tsx
+++ src/app/(afterLogin)/_component/FollowButton.tsx
@@ -3,5 +3,12 @@ type Props = {
 };
 
 export default function FollowButton({ onClick }: Props) {
-  return <button onClick={onClick}>팔로우</button>;
+  return (
+    <button
+      onClick={onClick}
+      className="text-followBtnSize text-white bg-grayBlack w-followBtnWidth h-inputHeight rounded-radius15"
+    >
+      팔로우
+    </button>
+  );
 }
