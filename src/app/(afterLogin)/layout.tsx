322f120d37dabafa3090a99c73d574227b97f40a
KimEunHye <gpqjr00@naver.com>
2024-05-02 22:47:37 +0900

    Feat: 유저 더미데이터 추가 및 팔로우 추천 영역 추가

--- src/app/(afterLogin)/layout.tsx
+++ src/app/(afterLogin)/layout.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from 'react';
+import { ReactNode, use } from 'react';
 import Image from 'next/image';
 
 import NavMenu from './_component/NavMenu';
@@ -6,12 +6,19 @@ import MonyangIcon from '/public/monyang-i.png';
 import Link from 'next/link';
 import RightSearch from './_component/RightSearch';
 import TitleBox from './_component/TitleBox';
+import FollowBox from './_component/FollowBox';
 
 type Props = {
   children: ReactNode;
 };
 
 export default function AfterLoginLayout({ children }: Props) {
+  const user = {
+    image: '/user_images.jpg',
+    userName: '아코집사',
+    userId: 'ako_cat',
+  };
+
   return (
     <div className="flex">
       <section className="grow">
@@ -39,8 +46,23 @@ export default function AfterLoginLayout({ children }: Props) {
       <section className="grow">
         <div className="px-padding15">
           <RightSearch />
-          <div className="w-searchWidth py-back mt-[64px]">
+          <div className="w-searchWidth py-back my-input">
             <TitleBox>팔로우 추천</TitleBox>
+            <FollowBox
+              image={user.image}
+              userName={user.userName}
+              userId={user.userId}
+            />
+            <FollowBox
+              image={user.image}
+              userName={user.userName}
+              userId={user.userId}
+            />
+            <FollowBox
+              image={user.image}
+              userName={user.userName}
+              userId={user.userId}
+            />
           </div>
         </div>
       </section>
