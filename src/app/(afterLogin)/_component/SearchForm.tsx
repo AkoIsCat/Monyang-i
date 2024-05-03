76cab38a276c28fcc51b6dd6e73234c3c6519ce5
KimEunHye <gpqjr00@naver.com>
2024-05-03 23:30:01 +0900

    Design: explore 페이지인 경우 다르게 나타나도록 수정

--- src/app/(afterLogin)/_component/SearchForm.tsx
+++ src/app/(afterLogin)/_component/SearchForm.tsx
@@ -1,12 +1,24 @@
+'use client';
+import { usePathname } from 'next/navigation';
+
 import SearchIcon from './SearchIcon';
 
 export default function SearchForm() {
+  const path = usePathname();
+
   return (
     <>
-      <form className="w-searchWidth h-searchHeight flex rounded-radius10 bg-lightBrownTP items-center px-right">
-        <SearchIcon />
-        <input className="text-inputSize bg-transparent outline-none mx-input h-searchInputHeight leading-searchInput grow" />
-      </form>
+      {path === '/explore' ? (
+        <form className="w-full h-searchHeight flex rounded-radius10 bg-lightBrownTP items-center px-right">
+          <SearchIcon />
+          <input className="text-inputSize bg-transparent outline-none mx-input h-searchInputHeight leading-searchInput grow" />
+        </form>
+      ) : (
+        <form className="w-searchWidth h-searchHeight flex rounded-radius10 bg-lightBrownTP items-center px-right">
+          <SearchIcon />
+          <input className="text-inputSize bg-transparent outline-none mx-input h-searchInputHeight leading-searchInput grow" />
+        </form>
+      )}
     </>
   );
 }
