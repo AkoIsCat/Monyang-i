b3648015b9d037c832b50da29f3d31b8e0ec4bb8
KimEunHye <gpqjr00@naver.com>
2024-05-03 23:30:21 +0900

    Design: explore 페이지인 경우 다르게 표시되도록 추가

--- src/app/(afterLogin)/_component/TrendBox.tsx
+++ src/app/(afterLogin)/_component/TrendBox.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useRouter } from 'next/navigation';
+import { useRouter, usePathname } from 'next/navigation';
 
 type Props = {
   trend: string;
@@ -7,16 +7,30 @@ type Props = {
 
 export default function TrendBox({ trend }: Props) {
   const router = useRouter();
+  const path = usePathname();
 
   const onClickTrend = (trend: string) => {
     router.push(`/search?q=${trend}`);
   };
 
   return (
-    <div className="w-searchWidth h-headerHeight border-b-1 border-lightBrownTP text-navSize flex items-center p-right">
-      <div className="cursor-pointer" onClick={() => onClickTrend(trend)}>
-        #{trend}
-      </div>
-    </div>
+    <>
+      {path === '/explore' ? (
+        <div className="w-full h-headerHeight border-b-1 border-lightBrownTP text-navSize flex items-center p-right">
+          <div
+            className="cursor-pointer hover:underline underline-offset-4"
+            onClick={() => onClickTrend(trend)}
+          >
+            #{trend}
+          </div>
+        </div>
+      ) : (
+        <div className="w-searchWidth h-headerHeight border-b-1 border-lightBrownTP text-navSize flex items-center p-right">
+          <div className="cursor-pointer" onClick={() => onClickTrend(trend)}>
+            #{trend}
+          </div>
+        </div>
+      )}
+    </>
   );
 }
