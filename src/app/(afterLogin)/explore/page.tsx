359e5ee6dfa5f817dcea9cf98b943c4a84922d06
KimEunHye <gpqjr00@naver.com>
2024-05-03 23:30:48 +0900

    Feat: explore 페이지 작성

--- src/app/(afterLogin)/explore/page.tsx
+++ src/app/(afterLogin)/explore/page.tsx
@@ -1,7 +1,22 @@
+import SearchForm from '../_component/SearchForm';
+import TrendBox from '../_component/TrendBox';
+
 export default function Explore() {
   return (
-    <div className="border-x-1 border-lightGray min-h-screen">
-      탐색 페이지 입니다.
+    <div className="border-x-1 border-lightGray min-h-screen flex w-full flex-col">
+      <div className="w-full p-[20px] border-b-10 border-darkBrown h-searchDivHeight">
+        <SearchForm />
+      </div>
+      <div className="w-full p-[20px] h-searchDivHeight">
+        <div className="w-full h-headerHeight border-b-1 border-lightBrownTP text-navSize flex items-center p-right text-6xl">
+          실시간 트렌드
+        </div>
+        <TrendBox trend="고양이" />
+        <TrendBox trend="고양이" />
+        <TrendBox trend="고양이" />
+        <TrendBox trend="고양이" />
+        <TrendBox trend="고양이" />
+      </div>
     </div>
   );
 }
