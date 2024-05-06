17dfcb2905a1579ad33cd0ac60df80235f65be79
KimEunHye <gpqjr00@naver.com>
2024-05-06 23:41:59 +0900

    Feat: 팔로우 여부에 따라 표시할 탭 생성

--- src/app/(afterLogin)/_component/Header.tsx
+++ src/app/(afterLogin)/_component/Header.tsx
@@ -1,9 +1,19 @@
-import { HeaderType } from "@/type";
+'use client';
+import { usePathname } from 'next/navigation';
+import { HeaderType } from '@/type';
 
 export default function Header({ headerText }: HeaderType) {
+  const path = usePathname();
+
   return (
     <div className="text-headerSize py-right px-[20px] border-b-10 border-darkBrown">
-      {headerText}
+      <div>{headerText}</div>
+      {path === '/home' && (
+        <div className="flex">
+          <div className="w-1/2 text-center text-trendSize">모두</div>
+          <div className="w-1/2 text-center text-trendSize">팔로우 중</div>
+        </div>
+      )}
     </div>
   );
 }
