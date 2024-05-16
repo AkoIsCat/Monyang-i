bd04a5c48e839ea98ef7d10fe046a4d2e3459dfc
KimEunHye <gpqjr00@naver.com>
2024-05-16 22:29:23 +0900

    Feat: 쪽지(Dm) 버튼 컴포넌트 작성

---
+++ src/app/(afterLogin)/_component/DmButton.tsx
@@ -0,0 +1,9 @@
+import MessageSmIcon from './MessageSmIcon';
+
+export default function DmButton() {
+  return (
+    <button className="border border-darkGray rounded-radiusCircle p-[5px]">
+      <MessageSmIcon />
+    </button>
+  );
+}
