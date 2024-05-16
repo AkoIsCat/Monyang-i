66273b5fa6c9bd89cbb42ef6fef13c03c5e0929f
KimEunHye <gpqjr00@naver.com>
2024-05-16 21:09:55 +0900

    Feat: 게시글 컴포넌트 우선 시험 적용

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,9 +1,11 @@
 import Header from '../_component/Header';
+import Posts from '../_component/Posts';
 
 export default function Home() {
   return (
     <div className="border-x-1 border-lightGray min-h-screen">
       <Header headerText="홈" />
+      <Posts />
     </div>
   );
 }
