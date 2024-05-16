aa83d8859755ef479e9b08fde8d52fc0204c96ff
KimEunHye <gpqjr00@naver.com>
2024-05-16 21:10:16 +0900

    Feat: Posts 컴포넌트 부분적 작성

--- src/app/(afterLogin)/_component/Posts.tsx
+++ src/app/(afterLogin)/_component/Posts.tsx
@@ -1,9 +1,37 @@
+import PostsUser from './PostsUser';
+import Image from 'next/image';
+import { faker } from '@faker-js/faker';
+
 export default function Posts() {
+  const postsUser = {
+    userName: '아코집사',
+    userId: 'ako_cat',
+    date: '2024-05-15 22:30:45',
+    image: '/user_images.jpg',
+    content: '안녕하세용 아코집사에요!',
+    contentImage: faker.image.avatar(),
+  };
+
   return (
     <div>
-      <div>프로필</div>
-      <div>내용</div>
-      <div>이미지</div>
+      <div>
+        <PostsUser
+          userName={postsUser.userName}
+          userId={postsUser.userId}
+          userImage={postsUser.image}
+          createDateAt={postsUser.date}
+        />
+      </div>
+      <div className="text-navSize m-input ">{postsUser.content}</div>
+      <div>
+        <Image
+          src={postsUser.contentImage}
+          alt="게시글 이미지"
+          priority
+          width={100}
+          height={100}
+        />
+      </div>
       <div>상태</div>
     </div>
   );
