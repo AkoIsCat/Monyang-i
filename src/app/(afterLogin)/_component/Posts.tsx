4450d2f5e15a7535af236e8937dd652f6a9ccbb5
KimEunHye <gpqjr00@naver.com>
2024-05-21 23:03:04 +0900

    Feat: home이나 search 페이지일 경우 게시글 하단에 구분선 추가

--- src/app/(afterLogin)/_component/Posts.tsx
+++ src/app/(afterLogin)/_component/Posts.tsx
@@ -1,6 +1,8 @@
+'use client';
 import PostsUser from './PostsUser';
 import Image from 'next/image';
 import { faker } from '@faker-js/faker';
+import { usePathname } from 'next/navigation';
 
 export default function Posts() {
   const postsUser = {
@@ -11,6 +13,8 @@ export default function Posts() {
     content: '안녕하세용 아코집사에요!',
     contentImage: faker.image.avatar(),
   };
+
+  const path = usePathname();
 
   return (
     <div>
@@ -23,15 +27,18 @@ export default function Posts() {
         />
       </div>
       <div className="text-navSize m-input ">{postsUser.content}</div>
-      <div className="relative">
+      {/* <div className="relative">
         <Image
           src={postsUser.contentImage}
           alt="게시글 이미지"
           priority
           layout="fill"
         />
-      </div>
+      </div> */}
       <div>상태</div>
+      {(path === '/home' || path === '/search') && (
+        <div className="text-headerSize py-right px-[20px] border-b-10 border-darkBrown" />
+      )}
     </div>
   );
 }
