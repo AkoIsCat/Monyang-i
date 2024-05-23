09aa4f98c077e48a7dbefc6c84e95489a5ac37ca
KimEunHye <gpqjr00@naver.com>
2024-05-23 20:49:30 +0900

    Feat: 리포스트 아이콘 svg 추가

---
+++ src/app/(afterLogin)/_component/RepostColorIcon.tsx
@@ -0,0 +1,41 @@
+export default function RepostColorIcon() {
+  return (
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 64 64"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className="cursor-pointer"
+    >
+      <path
+        d="M16 37.3333V24C16.0095 20.6109 17.0948 17.3124 19.0996 14.5799C21.1044 11.8474 23.9251 9.82202 27.155 8.79578C30.385 7.76955 33.8574 7.79544 37.0717 8.86973C40.286 9.94402 43.0762 12.0112 45.04 14.7733"
+        stroke="#5AB04F"
+        strokeWidth="5.33333"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M48 26.6667V40C47.9906 43.3891 46.9052 46.6876 44.9004 49.4201C42.8956 52.1526 40.075 54.178 36.845 55.2042C33.615 56.2305 30.1426 56.2046 26.9283 55.1303C23.714 54.056 20.9238 51.9888 18.96 49.2267"
+        stroke="#5AB04F"
+        strokeWidth="5.33333"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M21.3334 32L16 37.3333L10.6667 32"
+        stroke="#5AB04F"
+        strokeWidth="5.33333"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M42.6667 32L48 26.6667L53.3334 32"
+        stroke="#5AB04F"
+        strokeWidth="5.33333"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
---
+++ src/app/(afterLogin)/_component/RepostIcon.tsx
@@ -0,0 +1,41 @@
+export default function RepostIcon() {
+  return (
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className="cursor-pointer"
+    >
+      <path
+        d="M5 11.6667V7.5C5.00295 6.44092 5.34213 5.41013 5.96862 4.55622C6.59512 3.70231 7.47658 3.06938 8.48595 2.74869C9.49531 2.42799 10.5804 2.43608 11.5849 2.7718C12.5894 3.10751 13.4613 3.75351 14.075 4.61667"
+        stroke="#949494"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M6.66671 10L5.00004 11.6667L3.33337 10"
+        stroke="#949494"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M15 8.33334V12.5C14.9971 13.5591 14.6579 14.5899 14.0314 15.4438C13.4049 16.2977 12.5235 16.9306 11.5141 17.2513C10.5047 17.572 9.41961 17.5639 8.41514 17.2282C7.41067 16.8925 6.53874 16.2465 5.92505 15.3833"
+        stroke="#949494"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M13.3334 10L15 8.33334L16.6667 10"
+        stroke="#949494"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
